cce7cf610215beb3beff8a4f63c7b53b5bf69806
mistifiedwarrior <64316262+mistifiedwarrior@users.noreply.github.com>
2021-03-22 16:53:20 +0530

    updateOne operators

--- database.js
+++ database.js
@@ -7,11 +7,6 @@
 const {MongoClient, ObjectID} = require('mongodb');
 const connectionURL = process.env.connection_URL;
 const databaseName = 'task-manager';
-
-// const id = new ObjectID();
-// console.log(id.id.length);
-// console.log(id.toHexString().length);
-
 MongoClient.connect(
   connectionURL,
   {useNewUrlParser: true, useUnifiedTopology: true},
@@ -20,65 +15,88 @@ MongoClient.connect(
       return console.log('unable to connect mongodb database');
     }
     const db = client.db(databaseName);
-    // db.collection('users').findOne(
-    //   {_id: new ObjectID('6051d4ddc223231fb0b7d4b8')},
-    //   (error, user) => {
-    //     if (error) {
-    //       return console.log('unable to fetch');
-    //     }
-    //     console.log(user);
-    //   }
-    // );
-    db.collection('tasks').findOne(
-      {_id: new ObjectID('6053573e751bbd1a60c8c9fc')},
-      (error, description) => {
-        if (error) {
-          return console.log('unable to fetch');
+    db.collection('users')
+      .updateOne(
+        {_id: new ObjectID('6051d59e1cacdd0b50d558a3')},
+        {
+          // $set: {
+          //   name: 'AADII',
+          // },
+          $inc: {
+            age: -1,
+          },
         }
-        console.log(description);
-      }
-    );
-    db.collection('tasks')
-      .find({isCompleted: false})
-      .toArray((error, user) => {
-        console.log(user);
+      )
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log('Error!! ', error);
       });
-    // db.collection('users')
-    //   .find({age: 17})
-    //   .toArray((error, user) => {
-    //     console.log(user);
-    //   });
-    // db.collection('users')
-    //   .find({age: 17})
-    //   .count((error, count) => {
-    //     console.log(count);
-    //   });
-    // db.collection('users').insertOne(
-    //   {
-    //     // _id: id,
-    //     name: 'Aditya Gupta',
-    //     age: 11,
-    //   },
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log('unable to insert user');
-    //     }
-    //     console.log(result.ops);
-    //   }
-    // );
-
-    // db.collection('tasks').insertMany(
-    //   [
-    //     {description: 'Lunch', isCompleted: true},
-    //     {description: 'study', isCompleted: false},
-    //     {description: 'Exercise', isCompleted: true},
-    //   ],
-    //   (error, result) => {
-    //     if (error) {
-    //       return console.log('Unable to inserts tasks');
-    //     }
-    //     console.log(result.ops);
-    //   }
-    // );
   }
 );
+
+// ================== trial ========================//
+
+// db.collection('users').findOne(
+//   {_id: new ObjectID('6051d4ddc223231fb0b7d4b8')},
+//   (error, user) => {
+//     if (error) {
+//       return console.log('unable to fetch');
+//     }
+//     console.log(user);
+//   }
+// );
+
+// db.collection('users')
+//   .find({age: 17})
+//   .toArray((error, user) => {
+//     console.log(user);
+//   });
+// db.collection('users')
+//   .find({age: 17})
+//   .count((error, count) => {
+//     console.log(count);
+//   });
+// db.collection('users').insertOne(
+//   {
+//     // _id: id,
+//     name: 'Aditya Gupta',
+//     age: 11,
+//   },
+//   (error, result) => {
+//     if (error) {
+//       return console.log('unable to insert user');
+//     }
+//     console.log(result.ops);
+//   }
+// );
+
+// db.collection('tasks').insertMany(
+//   [
+//     {description: 'Lunch', isCompleted: true},
+//     {description: 'study', isCompleted: false},
+//     {description: 'Exercise', isCompleted: true},
+//   ],
+//   (error, result) => {
+//     if (error) {
+//       return console.log('Unable to inserts tasks');
+//     }
+//     console.log(result.ops);
+//   }
+// );
+
+// db.collection('tasks').findOne(
+//   {_id: new ObjectID('6053573e751bbd1a60c8c9fc')},
+//   (error, description) => {
+//     if (error) {
+//       return console.log('unable to fetch');
+//     }
+//     console.log(description);
+//   }
+// );
+// db.collection('tasks')
+//   .find({isCompleted: false})
+//   .toArray((error, user) => {
+//     console.log(user);
+//   });
